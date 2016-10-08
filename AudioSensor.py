a8a0af2930167209ae2a8f18b780389d0b971a8f
Matthew Wilson <mattwilsonmbw@gmail.com>
2016-10-08 02:15:57 -0600

    added to AudioSensor.py

--- AudioSensor.py
+++ AudioSensor.py
@@ -3,6 +3,8 @@
 import sys
 import socket
 import struct
+import sounddevice as sd
+from queue import Queue
 
 if len(sys.argv) != 4:
 	print('Usage: ip x y')
@@ -26,6 +28,28 @@ print('Waiting for acknowledgement')
 sock.recv(1)
 print('Starting transmission')
 
-while True:
-	# Send junk
-	sock.send(b'a')
+queue = Queue()
+
+def callback(indata, frames, time, status):
+	"""This is called (from a separate thread) for each audio block."""
+	if status:
+		print(status, flush=True)
+	# print ("TYPE = ",type(indata))
+	# print("INDATA == ", indata)
+	queue.put(indata)
+
+
+RATE = 44100
+
+CHANNELS = 1
+
+
+with sd.RawInputStream(samplerate=RATE, device=None, channels=CHANNELS, callback=callback, dtype='int32'):
+		print("#" * 80)
+		print("press Ctrl+C to stop the recording")
+		print("#" * 80)
+		while True:
+			print(queue.get())
+			sock.send(queue.get())
+
+	# sock.send(b'a')
