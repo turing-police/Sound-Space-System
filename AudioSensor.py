34d7e4d189df60001ba244f31358bec6c07b4201
Matthew Wilson <mattwilsonmbw@gmail.com>
2016-10-08 17:04:38 -0600

    added plotting to audio sensor

--- AudioSensor.py
+++ AudioSensor.py
@@ -7,9 +7,14 @@ import time
 import sounddevice as sd
 from queue import Queue
 
+plotting = False
+
 if len(sys.argv) != 5:
-	print('Usage: ip x y scale')
-	exit()
+	if (len(sys.argv) == "p"):
+		plotting = True
+	else:
+		print('Usage: ip x y scale')
+		exit()
 
 ip = sys.argv[1]
 port = 8888
@@ -33,7 +38,7 @@ while True:
 		print(err)
 		if(err.errno != TimeoutError):
 			time.sleep(0.5)
-			
+
 sock.settimeout(None)
 
 print('Sending coordinates')
@@ -45,13 +50,40 @@ print('Starting transmission')
 
 queue = Queue()
 
+
+if (plotting):
+	import numpy as np
+	import matplotlib.pyplot as plt
+	from drawnow import drawnow
+	times = Queue()
+	plt.ion() # enable interactivity
+	fig=plt.figure(1, figsize=(16,8)) # make a figure
+
+xList = []
+yList = []
+
+def makeFig():
+    plt.title("Raw Audio Signal")
+    plt.ylim(-1e8, 1e8)
+    # plt.scatter(xList, yList, c=yList, cmap="coolwarm")
+    plt.plot(xList, yList, 'r')
+    plt.xlim(xList[0], xList[-1])
+
+    plt.pause(1e-9)
+
+
 def callback(indata, frames, time, status):
 	"""This is called (from a separate thread) for each audio block."""
 	if status:
 		print(status, flush=True)
-	# print ("TYPE = ",type(indata))
-	# print("INDATA == ", indata)
-	queue.put(indata)
+
+	if (plotting):
+		time = np.linspace(time.inputBufferAdcTime, time.currentTime, 512)
+		times.put(time)
+		queue.put(np.frombuffer(indata, dtype='int32'))
+
+	else:
+		queue.put(indata)
 
 
 RATE = 44100
@@ -67,3 +99,8 @@ with sd.RawInputStream(samplerate=RATE, device=None, channels=CHANNELS, callback
 			data = queue.get()
 			print('Sent {0} samples.'.format(len(data)))
 			sock.send(data)
+
+			if (plotting):
+				xList = times.get()
+				yList = data
+				drawnow(makeFig)
