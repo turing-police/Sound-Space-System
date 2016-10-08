b9f944f3c33e4db61edfd8d419afb7ede9148cfc
crachmanin <crachmanin@gmail.com>
2016-10-08 13:16:49 -0600

    adding scale factor

--- sample.py
+++ sample.py
@@ -30,9 +30,9 @@ class SoundProcessor:
             #intensity0 = struct.unpack("I", stream0.readline(4))[0]
             #intensity1 = struct.unpack("I", stream1.readline(4))[0]
             #intensity2 = struct.unpack("I", stream2.readline(4))[0]
-            intensity0 = abs(arr0[i])
-            intensity1 = abs(arr1[i])
-            intensity2 = abs(arr2[i])
+            intensity0 = abs(arr0[i]) * client0.scale
+            intensity1 = abs(arr1[i]) * client1.scale
+            intensity2 = abs(arr2[i]) * client2.scale
             intensitySum = intensitySum + intensity0 + intensity1 + intensity2
             xytuple = self.processClients(client0, client1, client2, intensity0,
                                      intensity1, intensity2)
